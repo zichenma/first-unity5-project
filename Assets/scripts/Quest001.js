53cf36a1035d1ee0e463cc5d145582ff3827d279
Zichen Ma <mazichen1984@gmail.com>
2016-07-15 10:01:35 -0400

    07/14-E14

---
+++ Assets/scripts/Quest001.js
@@ -0,0 +1,40 @@
+﻿import UnityEngine.UI;
+
+var TextBoxOnCheck : int = 0;
+var MessageBox : GameObject;
+var TextBox : GameObject;
+var TextMessage : String;
+var QuestBox : GameObject;
+var QuestText : GameObject;
+var QuestName: String;
+
+function OnMouseDown (){
+    if (TextBoxOnCheck == 0){
+        TextBoxOnCheck = 1;
+        MessageBox.SetActive(true);
+        TextBox.GetComponent.<Text>().text = TextMessage;
+        QuestName = "Active Quest: 'Recover The Loot'";
+        QuestText.GetComponent.<Text>().text = QuestName;
+    }else{
+        TextBoxOnCheck = 0;
+        MessageBox.SetActive(false);
+        TextMessage = "Villager: Get going then!";
+    }
+}
+
+function Update () {
+    if (Input.GetButtonDown("Submit")){
+        if(TextBoxOnCheck == 1){
+            MessageBox.SetActive(false);
+            TextBoxOnCheck = 0;
+            TextMessage = "Villager: Get going then!";
+        }
+    }
+    if (Input.GetButtonDown("Cancel")){
+        if(TextBoxOnCheck == 1){
+            MessageBox.SetActive(false);
+            TextBoxOnCheck = 0;
+            TextMessage = "Villager: Get going then!";
+        }
+    }
+}
